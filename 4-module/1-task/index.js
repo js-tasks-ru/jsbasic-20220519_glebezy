cc04a6c041a65d07eda4a4f31ee19ec815810702
Троц Глеб Николаевич <gntrots@mts.ru>
2022-06-14 08:43:22 +0500

    Решил задачи 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    let list = document.createElement('ul')
+
+    friends.forEach(friend => {
+        let li = document.createElement('li');
+        li.innerHTML = friend.firstName + ' ' + friend.lastName;
+        list.append(li);
+    });
+
+    return list
 }
